571c02e0b25263c74179e1d3b7c06eac6160b4ef
Dilawar Khan <dev.dilawar@cruxlabs.com.au>
2026-03-14 06:24:33 -0400

    v1.0.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -121,6 +121,16 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         />
         <script
           suppressHydrationWarning
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "vvl5tu4i6r");`,
+          }}
+        />
+        <script
+          suppressHydrationWarning
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
         />
